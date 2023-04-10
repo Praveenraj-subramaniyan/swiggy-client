6f416dc965c1ea0082443ce746f537c161330ec5
spr887011 <spr887011@gmail.com>
2023-04-10 22:06:36 +0530

    icon added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import logo from "./logo.svg";
+import logo from "./images/swiggy.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="container-fluid">
-      <div className="row ">
-        <div className="col-sm-7 firstrow">col-6</div>
-        <div className="col-sm-5 imagefirstDiv firstrow">
-          <img
-            className="firstImage"
-            src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_1340/Lunch1_vlksgq"
-          />
+    <div>
+      <div className="container-fluid">
+        <div className="row ">
+          <div className="col-sm-7 firstrow">
+            <div className="container">
+              <div className="row">
+                <div className="col-sm-6">
+                  <img className="logo" src={logo} />
+                  <span>swiggy</span>
+                </div>
+                <div className="col-sm-6">.col-sm-3</div>
+                <div className="col-sm-6">.col-sm-3</div>
+                <div className="col-sm-6">.col-sm-3</div>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-5 imagefirstDiv firstrow">
+            <img
+              className="firstImage"
+              src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_1340/Lunch1_vlksgq"
+            />
+          </div>
         </div>
       </div>
     </div>
