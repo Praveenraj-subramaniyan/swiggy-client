ec44d701deaddb6106a3ac8ed6215688e3bf2506
spr887011 <spr887011@gmail.com>
2023-04-10 21:26:26 +0530

    Image added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-     Hi
+    <div className="container-fluid">
+      <div className="row ">
+        <div className="col-sm-7 firstrow">col-6</div>
+        <div className="col-sm-5 imagefirstDiv firstrow">
+          <img
+            className="firstImage"
+            src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_1340/Lunch1_vlksgq"
+          />
+        </div>
+      </div>
     </div>
   );
 }
