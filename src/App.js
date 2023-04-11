265387aba0410b9e28c217870e04005ff2b80e00
spr887011 <spr887011@gmail.com>
2023-04-11 18:18:29 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
   return (
     <div>
       <div className="container-fluid firstrow">
-        <div className="row =">
-          <div className="container col-sm-7  pr-5">
+        <div className="row ">
+          <div className="container col-sm-7 ">
             <div className="container firstrow mx-5 my-5">
-              <div className="row ">
+              <div className="row col-12 firstrow">
                 <div className="col-6">
                   <img className="logo" src={logo} />
                   <span className="Swiggycolour">swiggy</span>
@@ -46,15 +46,41 @@ function App() {
                     </div>
                   </div>
                 </div>
-                <p className="mt-5 greyColour">POPULAR CITIES IN INDIA</p>
+                <p className="mt-4 greyColour">POPULAR CITIES IN INDIA</p>
+                <p className="mt-3 cities">
+                  Ahmedabad <span className="greyColour">Bangalore</span>{" "}
+                  Chennai <span className="greyColour">Delhi</span> Gurgaon
+                  <span className="greyColour"> Hyderabad </span>
+                  Kolkata
+                  <span className="greyColour"> Mumbai </span> Pune & more.
+                </p>
               </div>
             </div>
           </div>
-          <div className="col-sm-5 imagefirstDiv ">
+          <div className="col-sm-5 imagefirstDiv firstrow">
             <img
               className="firstImage"
               src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_1340/Lunch1_vlksgq"
             />
+          </div>
+        </div>
+      </div>
+      <div className="container-fluid brownColour">
+        <div className="row">
+          <div className="col-sm-4">
+          <img className="" width="35%" height="199" src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_210,h_398/4x_-_No_min_order_x0bxuf"/>
+          <h6 className="mt-3">No Minimum Order</h6>
+          <p>Order in for yourself or for the group,<br/> with no restrictions on order value</p>
+          </div>
+          <div className="col-sm-4">
+          <img className="" width="112" height="206" src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_224,h_412/4x_Live_order_zzotwy"/>
+          <h6 className="mt-3">Live Order Tracking</h6>
+          <p>Know where your order is at all times,<br/> from the restaurant to your doorstep</p>
+          </div>
+          <div className="col-sm-4">
+          <img className="" width="124" height="188" src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_248,h_376/4x_-_Super_fast_delivery_awv7sn"/>
+          <h6 className="mt-3">Lightning-Fast Delivery</h6>
+          <p>Experience Swiggy's superfast delivery,<br/> for food delivered fresh & on time</p>
           </div>
         </div>
       </div>
