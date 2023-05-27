42029602c2f4cbf31e1e01dcd14df02b30c630d9
spr887011 <spr887011@gmail.com>
2023-05-27 19:45:24 +0530

    Cart.js added

---
+++ src/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Cart.css";
+import HomeHeader from './HomeHeader';
+
+function cart() {
+  return (
+    <div>
+      <HomeHeader highlight="cart" />
+    </div>
+  )
+}
+
+export default cart
