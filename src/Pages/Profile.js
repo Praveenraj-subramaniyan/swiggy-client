66a112f58289a31394aa0963f03128fdb6ca9916
spr887011 <spr887011@gmail.com>
2023-05-29 16:46:30 +0530

    Profile.js added

---
+++ src/Pages/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeHeader from "../Components/HomeHeader";
+
+function Profile() {
+  return (
+    <div>
+      <HomeHeader highlight="profile" />
+    </div>
+  );
+}
+
+export default Profile;
